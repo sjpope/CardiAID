7e0a505d0024d4ec4539654440fdd4d4b94966df
Samuel J Pope <spope@inrsenterprises.com>
2024-09-14 06:44:46 -0500

    Update settings.json and program.cpp. Splitting Forward/Backward chaining algo logic into separate helper files.

--- program.cpp
+++ program.cpp
@@ -1,18 +1,12 @@
+#include "backward_chain.h"
+#include "forward_chain.h"
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <map>
 
 using namespace std;
-
-int search_con(const string& variable);
-int rule_to_clause(int Ri);
-void update_VL(int Ci);
-void validate_Ri(int Ri, string& conclusion);
-void process(const string& variable);
-
-void initialize_knowledge_base();
-
 
 vector<string> conclusion_list;
 
@@ -23,21 +17,25 @@ map<string, string> variable_list;
 map<string, string> global_variable_list;
 
 
+void initialize_knowledge_base();
 
-int rule_to_clause(int Ri) {
+int main(){
 
     /*
-    This function will convert Rule # Ri to clause number, Ci, using the following formula. If the rule numbers are sequenced like 1,2,3,4,5, ……), formula is:
+    Identify the Goal variable (the variable whose value needs to be determined)
+ 
+    Call Process ( Goal variable)
+
+    After the program is complete, check if the derived global variable list contains, Goal variable and it is not null. 
+
+        If it does, 
+            that is the answer of processing the Back ward chaining. 
+            Print the value of the variable and pass it to Forward chaining program. 
         
-        CLAUSE NUMBER (Ci) = 4* (RULE NUMBER (Ri) - 1) + 1
+        If it is null, 
+            print a comment that Goal cannot be determined.
+        
+    */ 
 
-    If the rule numbers are sequenced like 10,20,30,40,50, ……), formula is:
-
-        CLAUSE NUMBER = 4* (RULE NUMBER  / 10 - 1) + 1
-
-    It has been assumed that four slots have been assigned for each rule in the Clause Variable list. If other than four has been assigned replace 4 by that number.
-    */
-
-   // We expect to sequence rule numbers like 1,2,3...
-    return 4 * (Ri - 1) + 1;
+    return 0;
 }
